bfd9822c6865c4f4670bb2f2d2f91bee27ee8025
jordan.bottrell@coretek.com <jordan.bottrell@coretek.com>
2020-10-19 23:28:02 -0400

    added westus data

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ def homepage():
 
 @app.route('/')
 def test():
-    response = requests.get("https://api.weather.gov/gridpoints/DTX/53,38/forecast")
+    response = requests.get("https://api.weather.gov/gridpoints/SEW/130,67/forecast")
     text = response.text
     json_string = json.loads(text)
     properties = json_string['properties']
@@ -43,7 +43,7 @@ def test():
     name5 = day5["name"]
     temp5 = day5["temperature"]
     cast5 = day5["shortForecast"]
-    return render_template("east.html", day1name=name1, day2name=name2, day3name=name3, day4name=name4, day5name=name5, day1temp=temp1, day2temp=temp2, day3temp=temp3, day4temp=temp4, day5temp=temp5, day1cast=cast1, day2cast=cast2, day3cast=cast3, day4cast=cast4,day5cast=cast5)
+    return render_template("west.html", day1name=name1, day2name=name2, day3name=name3, day4name=name4, day5name=name5, day1temp=temp1, day2temp=temp2, day3temp=temp3, day4temp=temp4, day5temp=temp5, day1cast=cast1, day2cast=cast2, day3cast=cast3, day4cast=cast4,day5cast=cast5)
 
 if __name__ == '__main__':
     app.run()
